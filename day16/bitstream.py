e858801b4910bd38e9d910e8460976492090933c
Florian <f.aumeier@me.com>
2021-12-23 21:04:08 +0100

    day16 parse all types of packages

--- day16/bitstream.py
+++ day16/bitstream.py
@@ -4,34 +4,53 @@ from dataclasses import dataclass
 @dataclass
 class Bitstream:
     bits: str
-    pos: int = 0
-
-    def decode_int(self, number_of_bits):
-        result = int(self.bits[self.pos : self.pos + number_of_bits], 2)
-        self.pos += number_of_bits
-        return result
-
-    def get_group(self, number_of_bits):
-        result = self.bits[self.pos : self.pos + number_of_bits]
-        self.pos += number_of_bits
-        return result
+    current_position: int = 0
 
     def decode_one_packet(self):
         version = self.decode_int(3)
         type_id = self.decode_int(3)
-        data = self.decode_literal_value_data()
+        data = self.decode_packet_data(type_id)
         return version, type_id, data
 
-    def decode_literal_value_data(self):
+    def decode_packet_data(self, type_id: int):
+        if type_id == 4:
+            return self.decode_literal_value_data()
+        return self.decode_operator_data()
+
+    def decode_int(self, number_of_bits):
+        return int(self.get_group(number_of_bits), 2)
+
+    def get_group(self, number_of_bits):
+        result = self.bits[
+            self.current_position : self.current_position + number_of_bits
+        ]
+        self.current_position += number_of_bits
+        return result
+
+    def decode_literal_value_data(self) -> int:
         group = self.get_group(5)
         bits = ""
         while group is not None:
             bits += group[1:]
-            next = self.get_group(5)
-            if next[0] == "0":
-                group = None
-                bits += next[1:]
-            else:
-                group = next
-
+            group = None if group[0] == "0" else self.get_group(5)
         return int(bits, 2)
+
+    def decode_operator_data(self):
+        length_of_type = self.decode_int(1)
+
+        if length_of_type == 1:
+            return self._decode_operator_packets_packets_by_count(self.decode_int(11))
+
+        return self._decode_operator_packets_by_length(self.decode_int(15))
+
+    def _decode_operator_packets_packets_by_count(self, count: int):
+        return [self.decode_one_packet() for _ in range(count)]
+
+    def _decode_operator_packets_by_length(self, length: int):
+        end = self.current_position + length
+        packets = []
+
+        while self.current_position < end:
+            packets.append(self.decode_one_packet())
+
+        return packets
--- day16/bitstream_test.py
+++ day16/bitstream_test.py
@@ -7,3 +7,10 @@ class TestBitstream:
 
         version, result, data = bit_stream.decode_one_packet()
         assert (version, result, data) == (6, 4, 2021)
+
+    def test_decode_operator_data(self):
+        bit_stream = Bitstream(
+            "11101110000000001101010000001100100000100011000001100000"
+        )
+        result = bit_stream.decode_one_packet()
+        assert result == (7, 3, [(2, 4, 1), (4, 4, 2), (1, 4, 3)])
