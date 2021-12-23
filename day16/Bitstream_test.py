8187db86271f4ece0849a354ea78791fa5a02577
Florian <f.aumeier@me.com>
2021-12-23 18:58:41 +0100

    day16 decode first packet

---
+++ day16/Bitstream_test.py
@@ -0,0 +1,9 @@
+from day16.Bitstream import Bitstream
+
+
+class TestBitstream:
+    def test_decode_packet(self):
+        bit_stream = Bitstream("110100101111111000101000")
+
+        version, result, data = bit_stream.decode_packet()
+        assert (version, result, data) == (6, 4, 2021)
